ef8e61b6d8d397888436dc48c3ca204bb48e6be5
mirfinisworking <mirfinisworking@gmail.com>
2015-04-23 12:12:01 +0800

    change models.py

--- account/models.py
+++ account/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class UserAccount(models.Model):
 	username = models.CharField(max_length = 40)
 	password = models.CharField(max_length = 20)
+
+class UserAdmin(models.ModelAdmin):
+	list_display = ('username','password')
+
+admin.site.register(UserAccount,UserAdmin)
