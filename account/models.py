6ec739f2166935a06e848ee09d2d53d59e132bde
mirfinisworking <mirfinisworking@gmail.com>
2015-04-25 15:02:29 +0800

    change account

--- account/models.py
+++ account/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class UserAccount(models.Model):
 	username = models.CharField(max_length = 40)
 	password = models.CharField(max_length = 20)
 
-class UserAdmin(models.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
 	list_display = ('username','password')
 
 admin.site.register(UserAccount,UserAdmin)
--- account/views.py
+++ account/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django import forms
-from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
 from account import models
 
 class UserForm(forms.Form):
@@ -23,8 +23,29 @@ def register(request):
 			user.username = username
 			user.password = password
 			user.save()
-			return render_to_response('success.html',{'username':username})
+			#return render_to_response('success.html',{'username':username})
+			return HttpResponseRedirect(reverse('success.html',{'username':username}))
 	else:
 			uf = UserForm()
 	return render_to_response('register.html',{'uf':uf})
+
+
+
+#	uf = UserForm(request.POST)
+#	try:
+#		uf.is_valid()
+#	except (KeyError, UserForm.DoesNotExist):
+#		return render(request, 'register.html',{
+#			'error_message': "You didn't input password or name"
+#			})
+#	else:
+#		username = uf.cleaned_data['username']
+#		password = uf.cleaned_data['password']
+#		user = models.User()
+#		user.username = username
+#		user.password = password
+#		user.save()
+#		return HttpResponseRedirect(reverse('success.html',{'username':username}))
+
+
 		
